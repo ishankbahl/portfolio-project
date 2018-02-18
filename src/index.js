f14754877db582576ce4cf266302005c4fa21225
ishankbahl <ishankbahl97@gmail.com>
2018-02-18 23:52:57 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Stream } from "./Components";
+import configureStore from "./stores/configureStore";
+import * as actions from "./actions";
+
+const tracks = [
+    {
+        title: "Some track",
+    },
+    {
+        title: "Some other track",
+    }
+];
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
+ReactDOM.render(
+    <Stream />,
+    document.getElementById("app")
+);
+
+module.hot.accept();
